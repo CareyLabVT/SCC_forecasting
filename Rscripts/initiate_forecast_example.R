33e9ee67f5c5b7a4d43abfbea5590e0c5606631a
rqthomas <rqthomas>
2018-07-20 13:52:57 -0400

    added code to run forecast function

---
+++ Rscripts/initiate_forecast_example.R
@@ -0,0 +1,2 @@
+source('/Users/quinn/Dropbox/Research/SSC_forecasting/SSC_forecasting/Rscripts/EnKF_GLM_wNOAAens_V2.R')
+run_forecast(first_day= '2018-07-08 00:00:00', sim_name = NA, hist_days = 3,forecast_days = 15)
